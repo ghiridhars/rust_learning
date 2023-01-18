7012a531521ccb294aaae8e56dff2d1fe6db1235
Ghiridhar S <57797796+ghiridhars@users.noreply.github.com>
2023-01-18 17:29:54 +0530

    first exercise

---
+++ exercise1.rs
@@ -0,0 +1,21 @@
+/*
+First task that was given - write comments
+// comment
+/* comment */
+/// Doc comments are parsed into HTML library docs //!
+
+*/
+fn main() {
+    println!("Hello, world!");
+}
+
+//Exercise 1
+// why rust : wanted to learn something  rerally tricky to boost my coder ego
+
+/* Exercise 2
+Parah on your fav TV show:
+Bojack : where to start on this? It is a story about a very sad horse that happens to be a famous movie / tv star.
+the whole of its universe is set up to be extremely relatable to current day human life and suffering.
+And the script of this show is just mind blowing. we can literally take words and sentences from the show and have a deep philosophical convo with ourselves.
+
+*/
