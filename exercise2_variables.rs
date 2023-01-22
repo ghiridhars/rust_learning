293c2b94054a9722c985e0f4f751eaa325330daf
Ghiridhar S <57797796+ghiridhars@users.noreply.github.com>
2023-01-22 09:16:47 +0530

    Exercise 2 for variables

---
+++ exercise2_variables.rs
@@ -0,0 +1,14 @@
+fn main(){
+    let word = "Fyodor Dostyevsky";// string
+    let num = "700"; // integer -- whole numbers [not decimals]
+    let is_val = true; //boolean
+    
+    println!("Books by {} is amazing.",word);
+    println!("Pages generally are in the range of {}s.",num);
+    println!("Above facts are {}.",is_val);
+// create 2 variables in main, assign one as false and other as 10 and pront using print macros
+    let switch = false;
+    let volume = 10;
+    println!("Switch is {} for volume {}", switch,volume);
+
+}
